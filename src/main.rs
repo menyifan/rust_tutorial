9c7aa22b70e416baf848643b5751a03f134b7dc8
Yifan Men <menyifan@msu.edu>
2022-12-27 21:34:17 +0800

    Second version

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("What is your name?");
+    println!("What is your name, please?");
     let mut name = String::new();
     let greeting = "Nice to meet you!";
     io::stdin().read_line(&mut name)

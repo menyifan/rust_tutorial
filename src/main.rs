867c3a99393a7b3e7e7aa074056ccdf66828b9c5
Yifan Men <menyifan@msu.edu>
2022-12-28 23:40:16 +0800

    Auto format

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 #![allow(unused)]
 
-use std::io;
 use rand::Rng;
-use std::io::{Write, BufReader, BufRead, ErrorKind};
-use std::fs::File;
 use std::cmp::Ordering;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
     println!("What is your name, please?");
     let mut name = String::new();
     let greeting = "Nice to meet you!";
-    io::stdin().read_line(&mut name)
+    io::stdin()
+        .read_line(&mut name)
         .expect("Didn't receive input");
     println!("Hello {}! {}", name.trim_end(), greeting);
 }
